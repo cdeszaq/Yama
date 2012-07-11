daee95a8cfc79c14f3ab5e4617b0e6b0328e5245
Rick Jensen <none@none>
2012-07-11 11:55:37 -0500

    Added the jsoup dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -36,6 +36,7 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.16'
 
         compile "org.jadira.usertype:usertype.jodatime:1.9"
+        compile "org.jsoup:jsoup:1.6.3"
     }
 
     plugins {
